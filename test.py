98f6dd2081c6994f96b17fb7d40d80a57d22fcbe
pi <mj_ol@outlook.com>
2018-08-28 14:43:13 +0000

    ADD:Add test file to show qr code

---
+++ test.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import numpy as np
+
+im = Image.open("QR.png",'r').convert('L')
+imdata = np.array(im)
+
+for i in range(45,405,10):
+    for j in range(45,405,10):
+        if imdata[i][j] == 0:
+            sys.stdout.write('  ')
+        else:
+            sys.stdout.write('\x1b[7m  \x1b[0m')
+        sys.stdout.write('\n')
+
