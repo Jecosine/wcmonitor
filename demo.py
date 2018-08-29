8ba865f9b0039139e4c5e2277dfb17748c2f02a5
pi <mj_ol@outlook.com>
2018-08-29 03:18:48 +0000

    UPDATE:send filehelper 'snapshot' and reply the photo

--- demo.py
+++ demo.py
@@ -2,12 +2,33 @@ import itchat
 import thread,os
 from itchat.content import TEXT
 from qrcode import printQR as pqr
+import cv2
+from PIL import Image
 
-@itchat.msg_register(TEXT)
-def _(msg):
-    print msg.fromUserName
-    print msg
-    print msg.user
+def get_snapshot():
+    capture = cv2.VideoCapture(0)
+    _, frame = capture.read()
+    cv2.imwrite('snap.jpg',frame)
+
+
+@itchat.msg_register(TEXT,)
+def text_reply(msg):
+    if msg.user.UserName == "filehelper":
+        print msg.Text
+        if "snapshot" in msg.Text:
+            try:
+                get_snapshot()
+                im = open('snap.jpg','rb')
+            except:
+                msg.user.send("Fail to get snap")
+            else:
+                msg.user.send_image(im)
+            im.close()
+        else:
+            msg.user.send("asdasdad")
+    #auth = itchat.search_friends(nickName = 'MJ')[0]
+    #auth.send("reply alter")
+    #return "balala"
 
 os.system("rm /home/pi/QR.png")
 thread.start_new_thread(pqr,())
