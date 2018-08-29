b2ce63c6ff512d5d3c52ec1045afec0272c0301b
pi <mj_ol@outlook.com>
2018-08-29 01:56:42 +0000

    UPDATE:Modified

--- demo.py
+++ demo.py
@@ -1,10 +1,16 @@
 import itchat
+import thread,os
 from itchat.content import TEXT
-import 
+from qrcode import printQR as pqr
+
 @itchat.msg_register(TEXT)
 def _(msg):
-    print msg.fromUsername
+    print msg.fromUserName
+    print msg
+    print msg.user
 
+os.system("rm /home/pi/QR.png")
+thread.start_new_thread(pqr,())
 itchat.auto_login()
 
 
